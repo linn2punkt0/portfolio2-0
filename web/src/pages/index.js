e31949812aadca1e876dd6d779ca73ddc73d3086
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-05-14 14:16:07 +0200

    added featured projects to homepage

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -9,15 +9,35 @@ import SEO from "../components/seo";
 import Layout from "../containers/layout";
 import styled from "styled-components";
 import FeaturedProjects from "../components/FeaturedProjects";
+import BlockContent from "@sanity/block-content-to-react";
+import LineBreakText from "../components/LineBreakText";
 
 export const query = graphql`
   query IndexPageQuery {
-    sanityHome {
+    projects: allSanityProject(
+      filter: { featured: { eq: true } }
+      limit: 3
+      sort: { fields: date, order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          title
+          mainImage {
+            asset {
+              url
+            }
+          }
+        }
+      }
+    }
+    homepage: sanityHome {
       name
       tagline
     }
   }
 `;
+
 const Styledindex = styled.div`
   width: 90vw;
   margin: 10vh auto;
@@ -66,29 +86,30 @@ const Styledindex = styled.div`
 `;
 const IndexPage = props => {
   const { data, errors } = props;
-  console.log(data);
 
-  // const site = (data || {}).site
-  // const projectNodes = (data || {}).projects
-  //   ? mapEdgesToNodes(data.projects)
-  //     .filter(filterOutDocsWithoutSlugs)
-  //     .filter(filterOutDocsPublishedInTheFuture)
-  //   : []
+  const homepage = (data || {}).homepage;
+  const projects = (data || {}).projects ? mapEdgesToNodes(data.projects) : [];
 
   return (
     <Layout>
       <Styledindex>
         {/* <SEO title={site.title} description={site.description} keywords={site.keywords} /> */}
         <div>
-          <h2>{data.sanityHome.name}</h2>
-          <h1>{data.sanityHome.tagline}</h1>
+          <h2>{data.homepage.name}</h2>
+          <h1>
+            <LineBreakText text={homepage.tagline} />
+          </h1>
         </div>
         <div className="featured">
           <h4>Featured Projects:</h4>
           <div className="row">
-            <FeaturedProjects title="title" imageUrl="" />
-            <FeaturedProjects title="title" imageUrl="" />
-            <FeaturedProjects title="title" imageUrl="" />
+            {projects.map(project => (
+              <FeaturedProjects
+                key={project.id}
+                title={project.title}
+                imageUrl={project.mainImage.asset.url}
+              />
+            ))}
           </div>
         </div>
       </Styledindex>
