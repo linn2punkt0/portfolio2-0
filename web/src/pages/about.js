f11fd8c788960d001e273c8a63cb3b017996c996
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-05-14 16:34:31 +0200

    adjusted icon size

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -46,9 +46,6 @@ const Styledabout = styled.div`
     font-size: 20px;
     line-height: 150%;
   }
-  img {
-    width: 70px;
-  }
   svg {
     fill: var(--main-accent-color);
   }
@@ -66,7 +63,7 @@ const about = props => {
           <p>
             <LineBreakText text={about.professional} />
           </p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 24 24">
             <path d="M12 4.419c-2.826-5.695-11.999-4.064-11.999 3.27 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-7.327-9.17-8.972-12-3.27z" />
           </svg>
           <p>
