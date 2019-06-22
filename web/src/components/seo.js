d29cf34e496f2fba37d54e4c5b4a691d0120f820
Linn2punkt0 <linn.johansson7@hotmail.com>
2019-06-22 19:53:48 +0200

    adding SEO-component

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -1,67 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
 
 function SEO({ description, lang, meta, keywords, title }) {
   return (
-    <StaticQuery
-      query={detailsQuery}
-      render={data => {
-        const metaDescription = description || (data.site && data.site.description) || "";
-        const siteTitle = (data.site && data.site.title) || "";
-        const siteAuthor = (data.site && data.site.author && data.site.author.name) || "";
-        return (
-          <Helmet
-            htmlAttributes={{ lang }}
-            title={title}
-            titleTemplate={title === siteTitle ? "%s" : `%s | ${siteTitle}`}
-            meta={[
-              {
-                name: "description",
-                content: metaDescription
-              },
-              {
-                property: "og:title",
-                content: title
-              },
-              {
-                property: "og:description",
-                content: metaDescription
-              },
-              {
-                property: "og:type",
-                content: "website"
-              },
-              {
-                name: "twitter:card",
-                content: "summary"
-              },
-              {
-                name: "twitter:creator",
-                content: siteAuthor
-              },
-              {
-                name: "twitter:title",
-                content: title
-              },
-              {
-                name: "twitter:description",
-                content: metaDescription
+    <Helmet
+      htmlAttributes={{ lang }}
+      title={title}
+      meta={[
+        {
+          name: "description",
+          content: description
+        },
+        {
+          property: "og:title",
+          content: title
+        },
+        {
+          property: "og:description",
+          content: description
+        },
+        {
+          property: "og:type",
+          content: "website"
+        },
+        {
+          name: "twitter:card",
+          content: "summary"
+        },
+        {
+          name: "twitter:title",
+          content: title
+        },
+        {
+          name: "twitter:description",
+          content: description
+        }
+      ]
+        .concat(
+          keywords && keywords.length > 0
+            ? {
+                name: "keywords",
+                content: keywords.join(", ")
               }
-            ]
-              .concat(
-                keywords && keywords.length > 0
-                  ? {
-                      name: "keywords",
-                      content: keywords.join(", ")
-                    }
-                  : []
-              )
-              .concat(meta)}
-          />
-        );
-      }}
+            : []
+        )
+        .concat(meta)}
     />
   );
 }
@@ -81,14 +65,3 @@ SEO.propTypes = {
 };
 
 export default SEO;
-
-const detailsQuery = graphql`
-  query DefaultSEOQuery {
-    site: sanitySiteSettings(_id: { eq: "siteSettings" }) {
-      title
-      description
-      keywords
-      author
-    }
-  }
-`;
--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -1,23 +1,29 @@
-import { graphql, StaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import React, { useState } from "react";
 import Burger from "../components/Burger";
 import Menu from "../components/Menu";
 import GlobalStyle from "../styles/GlobalStyle";
 import Footer from ".././components/Footer";
+import SEO from "../components/seo";
 
-/* const query = graphql`
+const query = graphql`
   query SiteTitleQuery {
     site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
       title
+      description
+      keywords
     }
   }
-`; */
+`;
 
 const LayoutContainer = ({ children }) => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
+  const { site } = useStaticQuery(query);
+
   return (
     <>
+      <SEO title={site.title} description={site.description} keywords={site.keywords} />
       <GlobalStyle />
       <Burger menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
       <Menu menuIsOpen={menuIsOpen} />
