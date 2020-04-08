c4cd75ce8ab9ed9ea02c57ee3df8e0f9423c9404
Linn <linn.johansson7@hotmail.com>
2020-04-08 12:07:26 +0200

    new lint setup

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,13 +1,36 @@
 module.exports = {
-  extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
-  rules: {
-    'react/prop-types': 0,
-    'object-curly-spacing': ['error', 'never']
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
   },
+  extends: ["airbnb", "prettier", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
   settings: {
-    react: {
-      pragma: 'React',
-      version: '16.8.4'
+    "import/resolver": {
+      node: {
+        paths: ["src"]
+      }
     }
+  },
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/prop-types": 0,
+    "no-param-reassign": 0,
+    "no-nested-ternary": 0,
+    "import/no-extraneous-dependencies": 0,
+    "react/jsx-props-no-spreading": 0
   }
-}
+};
